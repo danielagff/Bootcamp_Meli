73d939d8f06c3c78e367784a3840220c79aa77b8
danielagff <daniel.agff@gmail.com>
2024-12-05 00:07:40 -0300

    Adicionando o metodo de criar produto (criando apenas na slice global)

--- GO WEB - 2/aula-2/go.mod
+++ GO WEB - 2/aula-2/go.mod
@@ -2,4 +2,7 @@ module aula2gobases
 
 go 1.23.3
 
-require github.com/go-chi/chi v1.5.5
+require (
+	github.com/go-chi/chi v1.5.5
+	golang.org/x/exp v0.0.0-20241204233417-43b7b7cde48d
+)
--- GO WEB - 2/aula-2/main.go
+++ GO WEB - 2/aula-2/main.go
@@ -4,12 +4,13 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"golang.org/x/exp/rand"
 	"net/http"
 	"os"
 	"strconv"
-
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"time"
 )
 
 type Product struct {
@@ -36,6 +37,49 @@ func LoadJson() error {
 	return nil
 }
 
+func CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var requestBody Product
+	if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
+		panic(err)
+	}
+	if _, err := time.Parse("02/01/2006", requestBody.Expiration); err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+	requestBody.ID = max() + 1
+	requestBody.CodeValue = getProductCodeValue()
+	
+	ListOfProducts = append(ListOfProducts, &requestBody)
+
+	json.NewEncoder(w).Encode(requestBody)
+}
+
+func getProductCodeValue() string {
+	existsCode := make(map[string]bool)
+	for _, product := range ListOfProducts {
+		existsCode[product.CodeValue] = true
+	}
+	return generateProductCodeValue(existsCode)
+}
+
+func generateProductCodeValue(existingCodes map[string]bool) string {
+	rand.Seed(uint64(time.Now().UnixNano()))
+	const alphanum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	for {
+		code := ""
+		for i := 1; i <= 7; i++ {
+			code += string(alphanum[rand.Intn(len(alphanum))])
+		}
+
+		if !existingCodes[code] {
+			existingCodes[code] = true
+			return code
+		}
+	}
+}
+
 func GetProducts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(ListOfProducts)
@@ -52,7 +96,7 @@ func GetProductById(w http.ResponseWriter, r *http.Request) {
 
 func GetHigherPriceProductsByPrice(w http.ResponseWriter, r *http.Request) {
 	priceQParam := r.URL.Query().Get("price")
-	price, _ := strconv.ParseFloat(priceQParam,64)
+	price, _ := strconv.ParseFloat(priceQParam, 64)
 	listOfProducts := searchProductsByHigherPrice(price)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(listOfProducts)
@@ -65,6 +109,18 @@ func searchProductById(id int) *Product {
 		}
 	}
 	return nil
+}
+
+func max() int {
+	higherIdProduct := ListOfProducts[0]
+
+	for _, product := range ListOfProducts {
+		if product.ID > higherIdProduct.ID {
+			higherIdProduct = product
+		}
+	}
+
+	return higherIdProduct.ID
 }
 
 func searchProductsByHigherPrice(price float64) []Product {
@@ -94,6 +150,8 @@ func main() {
 	router.Get("/products/{id}", GetProductById)
 
 	router.Get("/products/search", GetHigherPriceProductsByPrice)
+
+	router.Post("/products", CreateProduct)
 
 	if err := http.ListenAndServe(":8081", router); err != nil {
 		panic(err)
